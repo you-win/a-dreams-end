2fcec5ef6311f2803091af731641aedd99952a14
Tim Yuen <timothy.yuen22@gmail.com>
2019-01-15 11:55:25 -0500

    Added code for spawn point

--- actors/spawn-point/SpawnPoint.gd
+++ actors/spawn-point/SpawnPoint.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
-export (Resource) var actor
+export (PackedScene) var actor
 
 func _ready():
-	pass
+	var instanced_actor = actor.instance()
+	instanced_actor.set_pos(self.position)
+	
+	get_tree().root.add_child(instanced_actor)
