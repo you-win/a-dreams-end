9e02acf0e303950463bd1dd54e5c0dcd3a2fe291
Tim Yuen <timothy.yuen22@gmail.com>
2019-04-30 21:16:33 -0400

    Fixed json serializaer

--- utils/WorldMapLoader.gd
+++ utils/WorldMapLoader.gd
@@ -11,15 +11,20 @@ func _ready() -> void:
 
 # If no argument is passed, loads the builtin world map
 func _init(map_to_load : String = "res://scenes/TestWorldMap.json") -> void:
-	# Should always return a dictionary
-	var parsed_json : JSONParseResult = JSON.parse(map_to_load)
+	var file = File.new()
+	file.open(map_to_load, File.READ)
 	
+	var json = file.get_as_text()
+	
+	var parsed_json : JSONParseResult = JSON.parse(json)
+	
+	# Should always return a dictionary
 	if typeof(parsed_json.result) == TYPE_DICTIONARY:
-		level_grid = _serialize_json_data()
+		level_grid = _serialize_json_data(parsed_json.result)
 		
 	else:
 		print("[ERROR] Improperly formatted map data.")
 
-func _serialize_json_data() -> LevelGrid:
+func _serialize_json_data(json : Dictionary) -> LevelGrid:
 	
 	return null
